df6b0961769a7db22a96088bba4772904535b5ab
OhSuYeong <wb0982@kyonggi.ac.kr>
2024-05-28 10:10:38 +0900

    24-05-28

--- lambda/bucket-creation/lambda_function.py
+++ lambda/bucket-creation/lambda_function.py
@@ -1,10 +1,54 @@
 import boto3
 import json
 from datetime import datetime
+import os
+import uuid
 
 # AWS 클라이언트 설정
 s3_client = boto3.client('s3')
 lambda_client = boto3.client('lambda')
+
+def configure_s3_bucket_notification(bucket_name):
+    # 현재 실행 중인 Lambda 함수의 ARN과 이름을 가져옴
+    lambda_name = os.environ['AWS_LAMBDA_FUNCTION_NAME']
+    lambda_region = os.environ['AWS_REGION']
+    lambda_account_id = boto3.client('sts').get_caller_identity().get('Account')
+    lambda_arn = f'arn:aws:lambda:{lambda_region}:{lambda_account_id}:function:{lambda_name}'
+    
+    # 고유한 StatementId 생성
+    statement_id = str(uuid.uuid4())
+    
+    # Lambda 권한 추가
+    s3_arn = f'arn:aws:s3:::{bucket_name}'
+    try:
+        response = lambda_client.add_permission(
+            FunctionName=lambda_name,
+            StatementId=statement_id,  # 고유한 Id 사용
+            Action='lambda:InvokeFunction',
+            Principal='s3.amazonaws.com',
+            SourceArn=s3_arn
+        )
+        print("Lambda permission response:", response)
+    except lambda_client.exceptions.ResourceConflictException as e:
+        print("Permission already exists, skipping add_permission.")
+    
+    # 알림 구성 생성
+    notification_configuration = {
+        'LambdaFunctionConfigurations': [
+            {
+                'LambdaFunctionArn': lambda_arn,
+                'Events': ['s3:ObjectRestore:Completed']
+            }
+        ]
+    }
+   
+    # 버킷 알림 설정
+    response = s3_client.put_bucket_notification_configuration(
+        Bucket=bucket_name,
+        NotificationConfiguration=notification_configuration
+    )
+   
+    return response
 
 def lambda_handler(event, context):
     #print(f'event ----> {event}')
@@ -22,6 +66,14 @@ def lambda_handler(event, context):
                     }
                 )
                 print(f"Bucket {bucket_name} created successfully.")
+                
+                # 버킷 생성 완료 대기
+                waiter = s3_client.get_waiter('bucket_exists')
+                waiter.wait(Bucket=bucket_name)
+                
+                response_config = configure_s3_bucket_notification(bucket_name)
+                
+                print(response_config)
 
                 # 두 번째 Lambda 함수 호출
                 invoke_response = lambda_client.invoke(
